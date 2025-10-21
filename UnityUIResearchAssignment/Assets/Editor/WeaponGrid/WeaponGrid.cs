6062a5421ae31bb7c75afec74cb1741b8e3c5afd
Daaanieell <151017261+Daaanieell@users.noreply.github.com>
2025-10-21 18:14:53 +0200

    start of dynamic grid with cs file

---
+++ UnityUIResearchAssignment/Assets/Editor/WeaponGrid/WeaponGrid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UIElements;
+
+public class WeaponGrid
+{
+    public VisualElement FillWeaponGrid()
+    {
+        var gridItem = new VisualElement();
+        gridItem.AddToClassList("grid-item");
+        return gridItem;
+    }
+}
---
+++ UnityUIResearchAssignment/Assets/Editor/WeaponGrid/WeaponGridMain.cs
@@ -0,0 +1,35 @@
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WeaponGridMain : EditorWindow
+{
+    [SerializeField] private VisualTreeAsset m_VisualTreeAsset = default;
+
+    private WeaponGrid Wg = new WeaponGrid();
+
+    [MenuItem("Tools/WeaponGridMain")]
+    public static void ShowExample()
+    {
+        WeaponGridMain wnd = GetWindow<WeaponGridMain>();
+        wnd.titleContent = new GUIContent("WeaponGridMain");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Instantiate UXML
+        VisualElement uxmlContent = m_VisualTreeAsset.Instantiate();
+        root.Add(uxmlContent);
+
+        var grid = uxmlContent.Q<ScrollView>("grid");
+        grid.Add(Wg.FillWeaponGrid());
+    }
+}
