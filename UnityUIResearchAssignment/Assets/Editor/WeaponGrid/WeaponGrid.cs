fbc44f98f4565ebc6aa7f3e2c3d45b5a84a66ea0
Daaanieell <151017261+Daaanieell@users.noreply.github.com>
2025-10-22 17:40:07 +0200

    cleaned code, grid is almost finished

--- UnityUIResearchAssignment/Assets/Editor/WeaponGrid/WeaponGrid.cs
+++ UnityUIResearchAssignment/Assets/Editor/WeaponGrid/WeaponGrid.cs
@@ -1,41 +1,60 @@
 ﻿using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class WeaponGrid
 {
-    private List<VisualElement> gridItems = new List<VisualElement>();
-
     private int maxRows = 5;
     private int maxColumns = 3;
 
-    private int count;
+    private int testAmountOfItems = 32;
 
+    private int count;
+    List<int> weaponPlaceholder = new List<int>();
+    
+    //TODO: needs parameters for List<Weapon>
     public VisualElement FillWeaponGrid()
     {
-        gridItems.Clear();
-
-        var gridContainer = new VisualElement();
+        //test
+        for (int i = 1; i <= testAmountOfItems; i++)
+        {
+            weaponPlaceholder.Add(i);
+        }
+        
+        VisualElement gridContainer = new VisualElement();
         gridContainer.AddToClassList("weapon-grid");
 
-        for (int row = 0; row < maxRows; row++)
+        for (int row = 0; row < weaponPlaceholder.Count; row++)
         {
-            var rowElement = new VisualElement();
+            VisualElement rowElement = new VisualElement();
             rowElement.AddToClassList("grid-row");
 
             for (int col = 0; col < maxColumns; col++)
             {
                 count++;
-                var item = new VisualElement();
+                if (count > weaponPlaceholder.Count) break;
+                
+                int itemNumber = count;
+                VisualElement item = new VisualElement();
                 item.AddToClassList("grid-item");
+                item.AddManipulator(new Clickable(() =>
+                {
+                    //TODO: this needs logic for equipping a weapon on an NPC
+                    //  maybe returning the weapon SO?
+                    Debug.Log("clicked number: " + itemNumber + " placeholder number: " + weaponPlaceholder[itemNumber-1]);
 
-                var name = new Label("itemName: " + count.ToString());
+                }));
+
+                VisualElement name = new Label("itemName: " + count.ToString());
                 item.Add(name);
 
                 rowElement.Add(item);
-                gridItems.Add(item);
             }
+
             gridContainer.Add(rowElement);
         }
+
         return gridContainer;
     }
 }
