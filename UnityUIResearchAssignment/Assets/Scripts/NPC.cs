d1f4fd31e0e070081faaafee0f273304be7a1fd9
Michelle <mba3810@students.fhv.at>
2025-10-23 15:15:58 +0200

    created NPC

---
+++ UnityUIResearchAssignment/Assets/Scripts/NPC.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private string name;
+    private int health;
+    private int maxHealth;
+    private string weaponType;
+    private Sprite weaponSprite;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
