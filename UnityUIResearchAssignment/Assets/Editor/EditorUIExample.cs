01bde0368c036dac125730960fa8e38c950e88df
bryanvonmeeteren <b.vonmeeteren@student.han.nl>
2025-10-23 21:51:12 +0200

    dropdown with the 3 weapon types, also added a label above it for clarity.

--- UnityUIResearchAssignment/Assets/Editor/EditorUIExample.cs
+++ UnityUIResearchAssignment/Assets/Editor/EditorUIExample.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UIElements;
 
 [CustomEditor(typeof(TestScript))]
@@ -7,13 +9,19 @@ public class EditorUIExample : Editor
 {
     public override VisualElement CreateInspectorGUI()
     {
+        var label = new Label("Weapon Selector");
+        label.style.unityFontStyleAndWeight = FontStyle.Bold;
+        label.style.marginBottom = 4;
+        label.style.unityTextAlign = TextAnchor.MiddleCenter;
+        
         var container = new VisualElement();
         var dropdown = new DropdownField();
+        
         dropdown.choices = new List<string> { "Melee", "Ranged", "Magic" };
         
-        dropdown.choices.Add("Telepathically");
         dropdown.value = "Melee";
         
+        container.Add(label);
         container.Add(dropdown);
         return container;
     }
